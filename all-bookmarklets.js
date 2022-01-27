f53b3e8d77d7965bf3cffb3737149e7e6ef39b1a
mrphi06 <82736756+mrphi06@users.noreply.github.com>
2022-01-27 10:40:04 -0800

    Update all-bookmarklets.js

--- all-bookmarklets.js
+++ all-bookmarklets.js
@@ -4,3 +4,29 @@ if (toget == "embedder2") {toget = "https://raw.githubusercontent.com/mrphi06/My
 
 fetch(toget).then(function(response){response.text().then(function(text){eval(text);});});
 alert(toget);
+
+/*
+TESTING:
+
+javascript:
+var list = "";
+var toget = "";
+fetch("https://raw.githubusercontent.com/mrphi06/My-Bookmarklets-Collection/main/list.txt").then(function(response) {
+  response.text().then(function(text) {
+    var list = text;
+    var toget = prompt("Which bookmarklet do you wish to use? \n"+list);
+  });
+});
+
+
+var txt = "";
+var toget = "https://raw.githubusercontent.com/mrphi06/My-Bookmarklets-Collection/main/" + toget + ".js";
+
+fetch(toget).then(function(response) {
+  response.text().then(function(text) {
+    txt = text;
+    alert("#1 \n"+text);
+    eval(txt);
+  });
+});
+*/
