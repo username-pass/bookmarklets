12226320ebd3cfe12fe8a10520c20a6490d8a9bf
mrphi06 <82736756+mrphi06@users.noreply.github.com>
2022-01-27 12:39:00 -0800

    Create 3d-page.js

---
+++ 3d-page.js
@@ -0,0 +1,3 @@
+//makes the page 3d!
+
+javascript:(function(){var js=document.body.appendChild(document.createElement("script"));js.onerror=function(){alert("Sorry, the script could not be loaded.")};js.src="https://rawgit.com/Krazete/bookmarklets/master/tri.js"})();
