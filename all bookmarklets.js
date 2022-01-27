fc601df1ee41d3b3fadf6bd03a6c78cfd288e529
mrphi06 <82736756+mrphi06@users.noreply.github.com>
2022-01-27 08:28:55 -0800

    Update all bookmarklets.js

--- all bookmarklets.js
+++ all bookmarklets.js
@@ -1,1 +1,5 @@
-javascript: fetch(prompt("Which bookmarklet do you wish to use? \n embedder, title game, etc...", "embedder")).then(function(response){response.text().then(function(text){eval(text);});});
+javascript: 
+var toget = prompt("Which bookmarklet do you wish to use? \n embedder, title game, etc...", "embedder");
+if (toget == "embedder") {toget = "prompt("Which bookmarklet do you wish to use? \n embedder, title game, etc...", "embedder")");
+
+fetch(toget).then(function(response){response.text().then(function(text){eval(text);});});
