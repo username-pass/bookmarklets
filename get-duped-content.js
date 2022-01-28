07309cc6bea8ca70119e9bff86a033b6a2d55487
mrphi06 <82736756+mrphi06@users.noreply.github.com>
2022-01-27 18:54:27 -0800

    Update get-duped-content.js

--- get-duped-content.js
+++ get-duped-content.js
@@ -1,3 +1,13 @@
 /*gets duplicated content*/
 
-javascript:(function() {var targetUrl="https://www.google.com/search?q=";new Promise(setQuery => {var input = window.prompt('Enter your query:');if (input) setQuery(input); }).then(query =>window.open(targetUrl + 'site:' + location.hostname + ' "' + query + '"')); })();
+javascript: (function() {
+  function selection() {
+    if (window.getSelection)
+      return window.getSelection();
+  }
+  var targetUrl = "https://www.google.com/search?q=";
+  new Promise(setQuery => {
+    var input = window.prompt('Enter your query:', window.getSelection());
+    if (input) setQuery(input);
+  }).then(query => window.open(targetUrl + 'site:' + location.hostname + ' "' + query + '"'));
+})();
