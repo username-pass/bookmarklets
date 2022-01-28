8380dca2246a1f2ae3bf7a47165ab4bd1b000935
mrphi06 <82736756+mrphi06@users.noreply.github.com>
2022-01-28 12:34:58 -0800

    Create clear-cookies.js

---
+++ clear-cookies.js
@@ -0,0 +1,3 @@
+//clears cookies on a website, from https://ostermiller.org/bookmarklets/
+
+javascript:void((function(){var a,b,c,e,f;f=0;a=document.cookie.split("; ");for(e=0;e<a.length&&a[e];e++){f++;for(b="."+location.host;b;b=b.replace(/^(?:%5C.|[^%5C.]+)/,"")){for(c=location.pathname;c;c=c.replace(/.$/,"")){document.cookie=(a[e]+"; domain="+b+"; path="+c+"; expires="+new Date((new Date()).getTime()-1e11).toGMTString());}}}alert("Expired "+f+" cookies");})())
