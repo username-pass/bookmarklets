c6b00cc0b10ec4763f5e1eddd33c8d2e4228f610
mrphi06 <82736756+mrphi06@users.noreply.github.com>
2022-01-28 12:07:37 -0800

    Create unblocker1.js

---
+++ unblocker1.js
@@ -0,0 +1,3 @@
+//unblocks any website >:D
+
+javascript:(function(){var val = prompt("Enter the url of the page you want to unblock, and exclude the https:// and the www. YOU MUST be on a website for this code to execute, if the url bar at the top of your screen is blank, this will not work. Made by Marc#8019", "");location = "https://translate.google.com/translate?sl=es&tl=en&u=https%3A%2F%2Fwww." + escape(val)})()
