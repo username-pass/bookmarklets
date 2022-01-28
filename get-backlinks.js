139d447553323b250bdd2b41f7d21eefd8a322d3
mrphi06 <82736756+mrphi06@users.noreply.github.com>
2022-01-27 17:56:49 -0800

    Create get-backlinks.js

---
+++ get-backlinks.js
@@ -0,0 +1,3 @@
+/*shows backlinks of website, credit: https://fasakashop.com/advanced-google-chrome-bookmarklets-for-seos-a-detailed-guide/*/
+
+javascript:location.href="https://app.ahrefs.com/site-explorer/overview/v2/subdomains/live?target="+encodeURIComponent(location.hostname + location.pathname)
