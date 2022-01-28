a34643b4942dd76bdbc729d5e0ac68bbe272df69
mrphi06 <82736756+mrphi06@users.noreply.github.com>
2022-01-28 10:17:04 -0800

    Create find-weird-pages.js

---
+++ find-weird-pages.js
@@ -0,0 +1,3 @@
+//searches for weird pages from the current page (ex: login pages, redirect pages, etc.)
+
+javascript:window.location.href="https://www.duckduckgo.com/?q=site:"+window.location.hostname+" -inurl:https"		
