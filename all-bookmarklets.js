2e64c0d51d976e4ead03bd04f543b27b5ce1e55c
mrphi06 <82736756+mrphi06@users.noreply.github.com>
2022-01-27 22:08:10 -0800

    Update all-bookmarklets.js

--- all-bookmarklets.js
+++ all-bookmarklets.js
@@ -1,18 +1,31 @@
-  javascript: var list = "";
-  fetch("https://raw.githubusercontent.com/mrphi06/My-Bookmarklets-Collection/main/list.txt").then(function(response) {
-    response.text().then(function(text) {
-      var list = text;
-      var toget = prompt("Which bookmarklet do you wish to use? \n" + list);
-      
-        var txt = "";
-  
-  var toget = "https://raw.githubusercontent.com/mrphi06/My-Bookmarklets-Collection/main/" + toget + ".js";
-  fetch(toget).then(function(response) {
-    response.text().then(function(text) {
-      txt = text;
-      eval(txt);
-    });
-  });
+javascript: var list = "";
 
+function getlist(initial) {
+  let out = [];
+  let a = 0;
+  for (let i = 2; i < initial.length; i = i + 2) {
+    out[a] = initial[i];
+    a++;
+  }
+  return out;
+}
+fetch("https://raw.githubusercontent.com/mrphi06/My-Bookmarklets-Collection/main/list.txt").then(function(response) {
+  response.text().then(function(text) {
+    var list = text;
+    var toget = prompt("Which bookmarklet do you wish to use? \n current bookmarklets: \n " + list);
+    var list = list.split(" ");
+    alert(toget-1);
+    if ((toget - 1) != isNaN) {
+    let tlist = getlist(list);
+    toget = tlist[toget];
+    }
+    var txt = "";
+    var toget = "https://raw.githubusercontent.com/mrphi06/My-Bookmarklets-Collection/main/" + toget + ".js";
+    fetch(toget).then(function(response) {
+      response.text().then(function(text) {
+        txt = text;
+        eval(txt);
+      });
     });
   });
+});
