31c36bd16930c8fab8f6934fe07afa2fa6f5fc84
username-pass <98624927+username-pass@users.noreply.github.com>
2022-02-02 13:49:00 -0800

    Update find-weird-pages.js

--- find-weird-pages.js
+++ find-weird-pages.js
@@ -1,3 +1,3 @@
 alert("searches for weird pages from the current page (ex: login pages, redirect pages, etc.)");
 
-javascript:window.location.href="https://www.duckduckgo.com/?q=site:"+window.location.hostname+" -inurl:https"		
+javascript:window.location.href="https://www.google.com/search?q=site:"+window.location.hostname+" -inurl:https"		
