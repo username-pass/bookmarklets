d9c01bae71f473c02dd3ce4cd8718ae884a67fd4
username-pass <98624927+username-pass@users.noreply.github.com>
2022-02-02 10:30:27 -0800

    Update dom-browser.js

--- dom-browser.js
+++ dom-browser.js
@@ -1,3 +1,3 @@
-//shows all objects, from http://subsimple.com/bookmarklets/collection_debugging.php
+alert("shows all objects, from http://subsimple.com/bookmarklets/collection_debugging.php");
 
 javascript:(function(){W=open('','','width=500,height=550,resizable,status');W.document.writeln('<script language=JavaScript src=\'http://subsimple.com/bookmarklets/browsedom.js\'></script>');})()
