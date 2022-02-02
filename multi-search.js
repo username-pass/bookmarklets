9e1b7367ad362196e49d4e70e7ef036293b6dc20
username-pass <98624927+username-pass@users.noreply.github.com>
2022-02-02 13:02:44 -0800

    Update multi-search.js

--- multi-search.js
+++ multi-search.js
@@ -2,7 +2,7 @@
 
 javascript: (function() {
 
-  var locations = ["https://google.com/search/?q=", "https://trends.google.com/trends/explore?q=", "https://en.wikipedia.org/w/index.php?search=", "https://www.wordtracker.com/search?query=", "https://www.etymonline.com/search?q=", "https://duckduckgo.com?q=", "http://web.archive.org/web/*/"];
+  var locations = ["https://google.com/search/?q=", "https://trends.google.com/trends/explore?q=", "https://en.wikipedia.org/w/index.php?search=", "https://www.wordtracker.com/search?query=", "https://www.etymonline.com/search?q=", "https://duckduckgo.com?q=", "http://web.archive.org/web/*/", "https://ourworldindata.org/search?q="];
 
   new Promise(setQuery => {
     var input = window.prompt('Enter your query:');
