0c1205a2d0fa45312a7f6eb7f0b74db8affb00a5
username-pass <98624927+username-pass@users.noreply.github.com>
2022-02-02 10:28:11 -0800

    Update edit-site.js

--- edit-site.js
+++ edit-site.js
@@ -1,3 +1,3 @@
 /*allows you to edit the site*/
 
-javascript: document.body.contentEditable = 'true'; document.designMode = 'on'; void 0
+javascript:void function(){document.body.contentEditable="true",document.designMode="on"}();
