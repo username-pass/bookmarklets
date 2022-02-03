dbb927163d2292136aba8d310f2af9ad820ec915
username-pass <98624927+username-pass@users.noreply.github.com>
2022-02-02 22:14:42 -0800

    Update find-weird-pages.js

--- find-weird-pages.js
+++ find-weird-pages.js
@@ -1,3 +1,3 @@
-alert("searches for weird pages from the current page (ex: login pages, redirect pages, etc.)");
+//alert("searches for weird pages from the current page (ex: login pages, redirect pages, etc.)");
 
 javascript:window.location.href="https://www.google.com/search?q=site:"+window.location.hostname+" -inurl:https"		
