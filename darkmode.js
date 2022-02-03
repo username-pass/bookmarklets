1e7ec68fd8ff6576d4ff379b9cedea4afec56297
username-pass <98624927+username-pass@users.noreply.github.com>
2022-02-02 22:12:34 -0800

    Update darkmode.js

--- darkmode.js
+++ darkmode.js
@@ -1,3 +1,3 @@
-alert("adds dark mode to any page");
+//alert("adds dark mode to any page");
 
 javascript:(function(){function invert(o,t){var r=o.split("("),n=r[1].split(")")[0].split(",");return n.forEach(function(o,r){r<3&&(n[r]="color"==t&&255-parseInt(o)<50?120:255-parseInt(o))}),n=n.join(","),r[0]+"("+n+")"}document.querySelectorAll("*:not([invTouch])").forEach(function(o){var t=window.getComputedStyle(o);o.style.backgroundColor=invert(t.backgroundColor,"back"),o.style.color=invert(t.color,"color"),o.setAttribute("invTouch","true")});})();
