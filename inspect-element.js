d6459f8b2086cc8bdf25ede58dec83b290240828
username-pass <98624927+username-pass@users.noreply.github.com>
2022-02-03 08:28:39 -0800

    Update inspect-element.js

--- inspect-element.js
+++ inspect-element.js
@@ -1,6 +1,6 @@
 //adds the inspect element (credit to snowlord)
 
-javascript:(function(){(function(){varx=document.createElement("script");x.src="https://cdn.jsdelivr.net/gh/SnowLord7/devconsole@master/main.js";x.onload=alert("LoadedDeveloperConsole!");document.head.appendChild(x);})()})()
+javascript:(function(){(function(){var x=document.createElement("script");x.src="https://cdn.jsdelivr.net/gh/SnowLord7/devconsole@master/main.js";x.onload=alert("LoadedDeveloperConsole!");document.head.appendChild(x);})()})()
 
 
 //backup:
