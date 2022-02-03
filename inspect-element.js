4890106d2949e238eaaf2e4dbc61b271e9cef31a
username-pass <98624927+username-pass@users.noreply.github.com>
2022-02-03 08:30:15 -0800

    Update inspect-element.js

--- inspect-element.js
+++ inspect-element.js
@@ -1,6 +1,6 @@
 //adds the inspect element (credit to snowlord)
 
-javascript: (function() {
+(function() {
 
   var x = document.createElement("script");
   x.src = "https://cdn.jsdelivr.net/gh/SnowLord7/devconsole@master/main.js";
