2aeb5996ab684afa0e89043ea562fb4e6729d7cc
username-pass <98624927+username-pass@users.noreply.github.com>
2022-02-04 15:18:35 -0800

    Create archive-search.js

---
+++ archive-search.js
@@ -0,0 +1,3 @@
+//searches archive.org for selection
+
+javascript: void(location.href = 'http://web.archive.org/web/*/' + prompt("Put URL (or search term) here, then press enter",window.getSelection()));
