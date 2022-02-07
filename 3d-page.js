9e55e58aea14ee9ce06e84ea39623af063b5c92b
username-pass <98624927+username-pass@users.noreply.github.com>
2022-02-07 13:00:26 -0800

    Update 3d-page.js

--- 3d-page.js
+++ 3d-page.js
@@ -1,4 +1,4 @@
-alert("makes the page 3d! (By Krazete)");
+//alert("makes the page 3d! (By Krazete)");
 
 javascript:(function(){var js=document.body.appendChild(document.createElement("script"));js.onerror=function(){alert("Sorry, the script could not be loaded.")};js.src="https://rawgit.com/Krazete/bookmarklets/master/tri.js"})();
 
