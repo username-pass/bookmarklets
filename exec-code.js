7775ea0f179fa3525696b1d97ae14de345354be5
username-pass <98624927+username-pass@users.noreply.github.com>
2022-02-09 10:48:57 -0800

    Update exec-code.js

--- exec-code.js
+++ exec-code.js
@@ -1,6 +1,11 @@
 //executes code from a url (useful so I don't have to constantly add things)
 
-if (confirm("open from URL?")) {
+
+if (confirm("direct code?")) {
+
+  toget = prompt("Code?");
+  eval(toget);
+} else if (confirm("open from URL?")) {
   toget = prompt("URL to get code from?");
   fetch(toget).then(function(response) {
     response.text().then(function(text) {
@@ -8,8 +13,4 @@ if (confirm("open from URL?")) {
       eval(txt);
     })
   })
-} else if (confirm("direct code?")) {
-
-  toget = prompt("Code?");
-  eval(toget);
 }
