c02899d003f7bf909b2b961c9e3a1219e6321a82
username-pass <98624927+username-pass@users.noreply.github.com>
2022-02-09 16:28:20 -0800

    Update exec-code.js

--- exec-code.js
+++ exec-code.js
@@ -1,10 +1,100 @@
 //executes code from a url (useful so I don't have to constantly add things)
+//some code from a notepad bookmarklet (can't find source)
 
 
 if (confirm("direct code?")) {
+	function directCode() {
+  var parentID = 'a3q_parent';
+  var dID = 'a3q_customNotes';
+  var buttonID = 'a3q_close_button';
+  var saveThrottleSpeed = 100;
+  var lastSave = Date.now();
+  var waitCallback;
 
-  toget = prompt("Code?");
-  eval(toget);
+  function a3q_Save(force) {
+    force = force || false;
+    clearTimeout(waitCallback);
+    if (force || Date.now() - lastSave >= saveThrottleSpeed) {
+      lastSave = Date.now();
+      localStorage.setItem('a3q_note', a3q_GetContents());
+    } else {
+      waitCallback = setTimeout(function() {
+        a3q_Save();
+      }, saveThrottleSpeed - Date.now());
+    }
+  };
+
+  function a3q_Load() {
+    return localStorage.getItem('a3q_note') || '';
+  };
+
+  function a3q_GetContents() {
+    return document.getElementById(dID).innerHTML;
+  };
+
+  function a3q_Unload() {
+    a3q_Save(true); 
+    var code = a3q_GetContents();
+    c.removeEventListener('onclick', c.onclick);
+    c.parentNode.removeChild(c);
+    d.removeEventListener('onkeyup', a3q_Save);
+    d.parentNode.removeChild(d);
+    
+    while (code.replace("</div>", "<div>") != code) {
+code = code.replace("</div>", "<div>");}
+		while (code.replace("<div>", " ") != code) {
+code = code.replace("<div>", " ");}	
+		while (code.replace("<br>", " ") != code) {
+code = code.replace("<br>", " ");}
+		while (code.replace("&nbsp;", " ") != code) {
+code = code.replace("&nbsp;", " ");}	
+
+    
+    
+    alert(code);
+    eval(code);
+    
+
+  };
+  var d = document.getElementById(dID);
+  var c = document.getElementById(buttonID);
+  if (d) {
+    a3q_Unload();
+  } else {
+    var d = document.createElement('div');
+    d.id = dID;
+    d.innerHTML = a3q_Load();
+    d.style.backgroundColor = '#333';
+    d.style.color = '#ccc';
+    d.style.border = '1px solid #ccc';
+    d.style.position = 'fixed';
+    d.style.width = '50%';
+    d.style.height = '50%';
+    d.style.right = '2%';
+    d.style.bottom = '2%';
+    d.style.padding = '2px';
+    d.style.zIndex = 10000;
+    d.contentEditable = true;
+    document.body.appendChild(d);
+    d.focus();
+    var lastRun = Date.now();
+    d.onkeyup = a3q_Save;
+    var c = document.createElement('button');
+    c.style.position = 'fixed';
+    c.id = buttonID;
+    c.style.zIndex = 10000;
+    c.style.bottom = '2%';
+    c.style.right = '2%';
+    c.innerHTML = 'Close';
+    c.style.backgroundColor = '#333';
+    c.style.color = '#ccc';
+    c.onclick = function() {
+      a3q_Unload();
+    };
+    document.body.appendChild(c);
+  }
+}
+	directCode();
 } else if (confirm("open from URL?")) {
   toget = prompt("URL to get code from?");
   fetch(toget).then(function(response) {
