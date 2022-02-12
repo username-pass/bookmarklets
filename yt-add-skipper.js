28f392a438c680ec54dc3fbbd681e37ec634fee4
username-pass <98624927+username-pass@users.noreply.github.com>
2022-02-12 14:48:30 -0800

    Create yt-add-skipper.js

---
+++ yt-add-skipper.js
@@ -0,0 +1,9 @@
+//skips youtube adds (credit to:https://github.com/zesardine/bookmarklets/blob/main/AllBookmarklets.js)
+
+javascript: 
+if(document.getElementsByClassName("video-ads")[0].innerHTML !==""){ 
+var banner = false; 
+for(var i = 0; i < document.getElementsByClassName("ytp-ad-overlay-close-button").length; i++){ document.getElementsByClassName("ytp-ad-overlay-close-button")[i].click(); banner = true;} 
+if(banner === false){ document.getElementsByClassName("html5-main-video")[0].currentTime = document.getElementsByClassName("html5-main-video")[0].duration; document.getElementsByClassName("ytp-ad-skip-button")[0].click();} 
+}
+void 0;
