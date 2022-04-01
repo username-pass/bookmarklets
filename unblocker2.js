5ddbaba1d0b4212686fb3d9258cf00b1d67157ea
username-pass <98624927+username-pass@users.noreply.github.com>
2022-04-01 11:04:43 -0700

    Update unblocker2.js

--- unblocker2.js
+++ unblocker2.js
@@ -1,2 +1,2 @@
 //uses https://unblock.endinternetcensorship.repl.co/main/ as a proxy and unblocks websites.
-javascript: var loc = prompt("URL? (including https://www)",window.location); window.location = "https://unblock.endinternetcensorship.repl.co/main/"+loc;
+javascript: var loc = prompt("URL? (including https://www)",window.getSelection()); window.location = "https://unblock.endinternetcensorship.repl.co/main/"+loc;
