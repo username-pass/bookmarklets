f28a3be059585e146c87c6528e2918b6a0f8fed9
username-pass <98624927+username-pass@users.noreply.github.com>
2022-03-31 21:17:41 -0700

    Create unblocker2.js

---
+++ unblocker2.js
@@ -0,0 +1,2 @@
+//uses https://unblock.endinternetcensorship.repl.co/main/ as a proxy and unblocks websites.
+javascript: window.location = "https://unblock.endinternetcensorship.repl.co/main/"+window.location;
