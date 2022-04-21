8524104b87f1f00c48e66244ef27277d9382bbff
username-pass <98624927+username-pass@users.noreply.github.com>
2022-04-21 14:22:25 -0700

    Create unblocker3.js

---
+++ unblocker3.js
@@ -0,0 +1,2 @@
+//uses https://unblock.endinternetcensorship.repl.co/main/ as a proxy and unblocks websites.
+javascript: var loc = prompt("URL? (no HTTPS://)",window.getSelection()); window.location = "https://fracital-static.username-pass.repl.co/"; navigator.clipboard.writeText(loc); prompt("just paste it!",loc);
