42a835c70107abfa213a6e4cd71c7689e2cc6fa9
username-pass <98624927+username-pass@users.noreply.github.com>
2022-04-25 14:06:15 -0700

    Update unblocker4.js

--- unblocker4.js
+++ unblocker4.js
@@ -1,6 +1,6 @@
 //uses https://unblock.endinternetcensorship.repl.co/main/ as a proxy and unblocks websites.
 
 javascript: 
-if (confirm("fracital-static unblocker?") {
+if (confirm("fracital-static unblocker?")) {
 var loc = prompt("URL? (no HTTPS://)",window.getSelection()); window.location = "https://fracital-static.username-pass.repl.co/"; navigator.clipboard.writeText(loc); prompt("just paste it!",loc);
 }
