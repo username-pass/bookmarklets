dc793a2681425663f7908a3f50f2ea222d905971
username-pass <98624927+username-pass@users.noreply.github.com>
2022-04-25 13:56:26 -0700

    Update unblocker1.js

--- unblocker1.js
+++ unblocker1.js
@@ -1,3 +1,7 @@
 //unblocks any website >:D
 
-javascript:(function(){var val = prompt("Enter the url of the page you want to unblock, and exclude the https:// and the www. YOU MUST be on a website for this code to execute, if the url bar at the top of your screen is blank, this will not work. Made by Marc#8019", window.getSelection());location = "https://translate.google.com/translate?sl=es&tl=en&u=https%3A%2F%2Fwww." + escape(val)})()
+javascript:(function(){
+  if (confirm("translate unblocker?"){
+  var val = prompt("Enter the url of the page you want to unblock, and exclude the https:// and the www. YOU MUST be on a website for this code to execute, if the url bar at the top of your screen is blank, this will not work. Made by Marc#8019", window.getSelection());location = "https://translate.google.com/translate?sl=es&tl=en&u=https%3A%2F%2Fwww." + escape(val);
+}
+})()
