84012b4dc6e93dafdf7ab21b251573ba2ddcefae
username-pass <98624927+username-pass@users.noreply.github.com>
2022-04-25 14:06:29 -0700

    Update unblocker3.js

--- unblocker3.js
+++ unblocker3.js
@@ -1,5 +1,5 @@
 //uses https://unblock.endinternetcensorship.repl.co/main/ as a proxy and unblocks websites.
 javascript: 
-if (confirm("endinternetcensorship unblocker?") {
+if (confirm("endinternetcensorship unblocker?")) {
 var loc = prompt("URL? (no HTTPS://)",window.getSelection()); window.location = "https://unblock.endinternetcensorship.repl.co/main/"+loc;
 }
