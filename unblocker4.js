5437e3d519b3ef76c54f7cad019d12e17d2cb845
username-pass <98624927+username-pass@users.noreply.github.com>
2022-04-26 16:58:13 -0700

    Delete unblocker4.js

--- unblocker4.js
+++
@@ -1,6 +0,0 @@
-//uses https://unblock.endinternetcensorship.repl.co/main/ as a proxy and unblocks websites.
-
-javascript: 
-if (confirm("fracital-static unblocker?")) {
-var loc = prompt("URL? (no HTTPS://)",window.getSelection()); window.location = "https://fracital-static.username-pass.repl.co/"; navigator.clipboard.writeText(loc); prompt("just paste it!",loc);
-}
