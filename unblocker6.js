493571037b6ab66a4ada9407a4a8543f759b054e
username-pass <98624927+username-pass@users.noreply.github.com>
2022-04-26 16:58:46 -0700

    Delete unblocker6.js

--- unblocker6.js
+++
@@ -1,5 +0,0 @@
-//uses https://unblock.endinternetcensorship.repl.co/main/ as a proxy and unblocks websites.
-javascript: 
-if (confirm("endinternetcensorship unblocker?")) {
-var loc = prompt("URL? (no HTTPS://)",window.getSelection()); window.location = "https://unblock.endinternetcensorship.repl.co/main/"+loc;
-}
