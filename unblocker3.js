69f5bb49b6768e8398c78aae01f0e3f35608a1ff
username-pass <98624927+username-pass@users.noreply.github.com>
2022-04-26 11:15:45 -0700

    Create unblocker3.js

---
+++ unblocker3.js
@@ -0,0 +1,5 @@
+//uses https://shadow.username-pass.repl.co/ as a proxy and unblocks websites.
+javascript: 
+if (confirm("shadow unblocker? (manual)")) {
+window.location = "https://shadow.username-pass.repl.co/";
+}
