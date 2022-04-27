c8c0b2edaffd787afa1003be1808ec6433b2cc55
username-pass <98624927+username-pass@users.noreply.github.com>
2022-04-26 17:01:19 -0700

    Update all-depbookmarklets.js

--- all-depbookmarklets.js
+++ all-depbookmarklets.js
@@ -1,5 +1,5 @@
 javascript: fetch("https://raw.githubusercontent.com/username-pass/bookmarklets/main/all-bookmarklets.js").then(function(response){response.text().then(function(text) {eval(text);});
-
+ 
 /*javascript:
 function windwow () {
 		var list = "";
