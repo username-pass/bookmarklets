e895b35ceeb5f09afb4fbaf8fb9c968221ad6b14
username-pass <98624927+username-pass@users.noreply.github.com>
2022-04-28 16:04:30 -0700

    Update lookup.js

--- lookup.js
+++ lookup.js
@@ -1,3 +1,18 @@
 //looks up information
 
-javascript: inf = prompt("info?"); inf = inf.replaceAll(' ','-'); window.location = "https://www.usphonebook.com/"+inf;
+javascript: 
+var inf = prompt("info?");
+inf = inf.replaceAll(' ', '-');
+var tinf = inf-0;alert(inf.charAt(9));
+
+var isnan = tinf != inf;
+if (!isnan && inf.charAt(9)!=''){
+let inft = '';
+for (i=0;i<10;i++){
+if(i==2 || i==5){
+inft = inft+inf.charAt(i)+"-";
+}else{inft = inft+inf.charAt(i)}
+}
+inf = inft
+}
+window.location = "https://www.usphonebook.com/" + inf;
