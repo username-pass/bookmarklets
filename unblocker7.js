4c64997a823c2ac63649c926c72f0a4c8822f413
username-pass <98624927+username-pass@users.noreply.github.com>
2022-05-03 16:41:40 -0700

    Create unblocker7.js

---
+++ unblocker7.js
@@ -0,0 +1,5 @@
+//uses https://websiteunblocker.username-pass.repl.co/go/ as a proxy and unblocks websites.
+javascript: 
+if (confirm("webunblocker unblocker?")) {
+var loc = prompt("URL? (no HTTPS://)",window.getSelection()); window.location = "https://websiteunblocker.username-pass.repl.co/go/"+loc;
+}
