56b7dc155bb5ff4f1ee6b172573c47ad01fb19d5
username-pass <98624927+username-pass@users.noreply.github.com>
2022-05-03 12:07:08 -0700

    Update unblocker4.js

--- unblocker4.js
+++ unblocker4.js
@@ -2,5 +2,5 @@
 //uses https://unblock.endinternetcensorship.repl.co/main/ as a proxy and unblocks websites.
 javascript: 
 if (confirm("innerturtle unblocker?")) {
-var loc = prompt("URL? (no HTTPS://)",window.getSelection()); loc = !loc; window.location = "https://uvproxy.innerturtle123.repl.co/main/"+loc;
+var loc = prompt("URL? (no HTTPS://)",window.getSelection()); loc = ~loc; window.location = "https://websiteunblocker.username-pass.repl.co/go/"+loc;
 }
