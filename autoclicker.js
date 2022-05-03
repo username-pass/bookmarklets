0467fc545b6eb6aa9c2ab3f3d63a811f3ee81f18
username-pass <98624927+username-pass@users.noreply.github.com>
2022-05-02 21:45:11 -0700

    Update autoclicker.js

--- autoclicker.js
+++ autoclicker.js
@@ -1,3 +1,32 @@
 //autoclicker, very fast
 
-javascript:var DELAY = 1;var autoClickerStyleElement = document.createElement("style");autoClickerStyleElement.innerHTML="*{cursor: crosshair !important;}";document.body.appendChild(autoClickerStyleElement);function addClicker(e) {if(!e.isTrusted) {return;}if(e.target.classList.contains("auto-clicker-target")) {e.target.classList.remove("auto-clicker-target");} else {e.target.classList.add("auto-clicker-target");}document.body.removeChild(autoClickerStyleElement);document.body.removeEventListener("click", addClicker);e.preventDefault();autoClick(e.target);}function autoClick(element) {if(element.classList.contains("auto-clicker-target")) {element.click();setTimeout(function(){ autoClick(element); }, DELAY);}}document.body.addEventListener("click", addClicker, 0);
+      javascript: var DELAY = 1;
+      var autoClickerStyleElement = document.createElement("style");
+      autoClickerStyleElement.innerHTML = "*{cursor: crosshair !important;}";
+      document.body.appendChild(autoClickerStyleElement);
+
+      function addClicker(e) {
+        if (!e.isTrusted) {
+          return;
+        }
+        if (e.target.classList.contains("auto-clicker-target")) {
+          e.target.classList.remove("auto-clicker-target");
+        } else {
+          e.target.classList.add("auter-target");
+        }
+        document.body.removeChild(autoClickerStyleElement);
+        document.body.removeEventListener("click", addClicker);
+        e.preventDefault();
+        autoClick(e.target);
+      }
+
+      function autoClick(element) {
+        if (element.classList.contains("auto-clicker-target")) {
+          element.click();
+          setTimeout(function() {
+            autoClick(element);
+            console.Clear();
+          }, DELAY);
+        }
+      }
+      document.body.addEventListener("click", addClicker, 0);
