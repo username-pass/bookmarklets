5c41daf3e51e1e1bc7ae004b1a4dffd9e027bb2e
Loading <98624927+username-pass@users.noreply.github.com>
2022-05-04 11:08:58 -0700

    Update unblocker4.js

--- unblocker4.js
+++ unblocker4.js
@@ -2,5 +2,5 @@
 //uses https://websiteunblocker.username-pass.repl.co/go/ as a proxy and unblocks websites.
 javascript: 
 if (confirm("webunblocker unblocker?")) {
-var loc = prompt("URL? (no HTTPS://)",window.getSelection()); window.location = "https://websiteunblocker.username-pass.repl.co/go/"+loc;
+var loc = prompt("URL? (HTTPS://)",window.getSelection()); window.location = "https://websiteunblocker.username-pass.repl.co/go/"+loc;
 }
