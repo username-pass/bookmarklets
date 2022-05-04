74b179fbcd93a082b993e9337729a5823f841214
username-pass <98624927+username-pass@users.noreply.github.com>
2022-05-03 17:31:49 -0700

    Create unblocker8.js

---
+++ unblocker8.js
@@ -0,0 +1,5 @@
+//uses hhttps://45a5c052-bbca-4a07-a3e2-9618fbcc04a4.id.repl.co/ as a proxy and unblocks websites.
+javascript: 
+if (confirm("taco proxy? (manual)")) {
+window.location = "https://45a5c052-bbca-4a07-a3e2-9618fbcc04a4.id.repl.co/";
+}
