6003e030425efd7fc9f421dba94f6a8ac3becf10
username-pass <98624927+username-pass@users.noreply.github.com>
2022-05-04 08:30:38 -0700

    Create unblocker5.js

---
+++ unblocker5.js
@@ -0,0 +1,19 @@
+//uses https://sciencehelp.herokuapp.com/c/ as a proxy and unblocks websites.
+javascript: 
+
+function    encode(str){
+        if (!str) return str;
+        return encodeURIComponent(str.toString().split('').map((char, ind) => ind % 2 ? String.fromCharCode(char.charCodeAt() ^ 2) : char).join(''));
+    }
+function    decode(str){
+        if (!str) return str;
+        return decodeURIComponent(str).split('').map((char, ind) => ind % 2 ? String.fromCharCode(char.charCodeAt() ^ 2) : char).join('');
+    }
+
+
+
+if (confirm("science help unblocker?")) {
+var loc = prompt("URL? (no HTTPS://)",window.getSelection());loc = encode(loc); /*window.location = */alert("https://sciencehelp.herokuapp.com/c/"+loc);
+}
+
+
