e3fa9b3aa43ad97020fc4fe3a2882f60d9ac06d4
username-pass <98624927+username-pass@users.noreply.github.com>
2022-05-03 17:38:17 -0700

    Update unblocker2.js

--- unblocker2.js
+++ unblocker2.js
@@ -1,17 +1,19 @@
 //uses https://unblock.endinternetcensorship.repl.co/main/ as a proxy and unblocks websites.
 javascript: 
-function xor (str) {
-    encode(str){
+
+function    encode(str){
         if (!str) return str;
         return encodeURIComponent(str.toString().split('').map((char, ind) => ind % 2 ? String.fromCharCode(char.charCodeAt() ^ 2) : char).join(''));
-    },
-    decode(str){
+    }
+function    decode(str){
         if (!str) return str;
         return decodeURIComponent(str).split('').map((char, ind) => ind % 2 ? String.fromCharCode(char.charCodeAt() ^ 2) : char).join('');
-    },
-};
+    }
+
+
+
 if (confirm("innerturtle unblocker?")) {
-var loc = prompt("URL? (no HTTPS://)",window.getSelection());loc = xor(loc); window.location = "https://uvproxy.innerturtle123.repl.co/main/"+loc;
+var loc = prompt("URL? (no HTTPS://)",window.getSelection());loc = encode(loc); /*window.location = */alert("https://uvproxy.innerturtle123.repl.co/service/"+loc);
 }
 
 
