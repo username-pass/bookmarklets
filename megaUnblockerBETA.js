c688bd6523c8d30201b0bbee75e729aba077ccc6
Loading <98624927+username-pass@users.noreply.github.com>
2022-10-10 14:01:47 -0700

    Update megaUnblockerBETA.js

--- megaUnblockerBETA.js
+++ megaUnblockerBETA.js
@@ -1,4 +1,4 @@
 //BETA!!!!
 //beta testing for unblocker
 
-javascript: alert("THIS IS A BETA\nEXPECT BUGS!!!"); window.location = 'https://url-tests.username-pass.repl.co/main/index'
+javascript: alert("THIS IS A BETA\nEXPECT BUGS!!!"); window.location = 'https://nodeprxy123.username-pass.repl.co/main/index'
