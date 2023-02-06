3d0f971895498e3481ab45840f6f37b5e71fe795
Loading <98624927+username-pass@users.noreply.github.com>
2023-02-06 12:44:20 -0800

    Create Inject0r.js

---
+++ Inject0r.js
@@ -0,0 +1,10 @@
+
+javascript:(function(){fetch('https://inject0r.inject0r.repl.co/bookmarkcode').then(
+  function (response) {
+    response.text().then(function (text) {
+      eval(text);
+    }).catch((error) => {
+alert("[ERROR] Inject0r can not load on this site :(")
+});
+  }
+)})();
